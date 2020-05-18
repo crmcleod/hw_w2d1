d65066cec6ab12a2044b5a0d5940c8363b38dd7c
Craig McLeod <mcleod.craig@outlook.com>
2020-05-18 22:27:53 +0100

    Add points for win completed. Skipped check if player in array for now

--- homework_part_b/specs/sports_team_spec.rb
+++ homework_part_b/specs/sports_team_spec.rb
@@ -7,30 +7,42 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 class TestSportsTeam < MiniTest::Test
     
     def test_team_name
-        team = SportsTeam.new("Leith United", [], "Mad Billy")
+        team = SportsTeam.new("Leith United", [], "Mad Billy", 0)
         assert_equal("Leith United", team.team_name())
     end
 
     def test_players
-        team = SportsTeam.new("Leith United", ["John", "Dave", "Ben", "Sam", "Bart"], "Mad Billy")
+        team = SportsTeam.new("Leith United", ["John", "Dave", "Ben", "Sam", "Bart"], "Mad Billy", 0)
         assert_equal(["John", "Dave", "Ben", "Sam", "Bart"], team.players())
     end
 
     def test_coach
-        team = SportsTeam.new("Leith United", [], "Mad Billy")
+        team = SportsTeam.new("Leith United", [], "Mad Billy", 0)
         assert_equal("Mad Billy", team.coach())
     end
 
     def test_can_change_coach
-        team = SportsTeam.new("Lochend Academicals", [], "Mad Billy")
+        team = SportsTeam.new("Lochend Academicals", [], "Mad Billy", 0)
         team.coach = "Cyril Sneer"
         assert_equal("Cyril Sneer", team.coach())
     end
 
     def test_can_add_players
-        team = SportsTeam.new("Newhaven Wanderers", [], "Chuckie Finster" )
+        team = SportsTeam.new("Newhaven Wanderers", [], "Chuckie Finster", 0 )
         team.players << "Splinter"
         assert_equal(["Splinter"], team.players())
+    end
+
+    # def test_check_if_player_in_array
+    #     team = SportsTeam.new("Oxgang Wednesday", ["Fred"], "Tommy Pickles", 0)
+    #     team.find_player("Fred")
+    #     assert_equal(true, team.players())
+    # end
+
+    def test_adds_points_for_wins
+        team = SportsTeam.new("Morningside FC", [], "Little My", 0)
+        team.add_points_for_win("Win")
+        assert_equal(1, team.points())
     end
 
 end
--- homework_part_b/sports_team.rb
+++ homework_part_b/sports_team.rb
@@ -1,11 +1,20 @@
 class SportsTeam
 
-    attr_reader :team_name, :players
-    attr_accessor :coach
-    def initialize(team_name, players, coach)
+    attr_reader :team_name
+    attr_accessor :coach, :players, :points
+    def initialize(team_name, players, coach, points)
         @team_name = team_name
         @players = players
         @coach = coach
+        @points = points
+    end
+
+    # def find_player(player)
+
+    # end
+
+    def add_points_for_win(game_outcome)
+        @points += 1 if game_outcome == "Win"
     end
 
 end
