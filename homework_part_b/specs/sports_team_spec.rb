c4051c3ebd3f48e707cd66b494eaf1fb65a855ff
Craig McLeod <mcleod.craig@outlook.com>
2020-05-18 20:36:35 +0100

    Defined test players in spec

--- homework_part_b/specs/sports_team_spec.rb
+++ homework_part_b/specs/sports_team_spec.rb
@@ -7,8 +7,13 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 class TestSportsTeam < MiniTest::Test
     
     def test_team_name
-        team = SportsTeam.new("Leith United", "100", "Mad Billy")
+        team = SportsTeam.new("Leith United", [], "Mad Billy")
         assert_equal("Leith United", team.team_name())
+    end
+
+    def test_players
+        team = SportsTeam.new("Leith United", ["John", "Dave", "Ben", "Sam", "Bart"], "Mad Billy")
+        assert_equal(["John", "Dave", "Ben", "Sam", "Bart"], team.players())
     end
 
 end
