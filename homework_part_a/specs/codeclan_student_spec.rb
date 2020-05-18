1cf2882a3d72da59ccd5d45dc21431d6aea8df4c
Craig McLeod <mcleod.craig@outlook.com>
2020-05-18 16:00:09 +0100

    Initial commit

---
+++ homework_part_a/specs/codeclan_student_spec.rb
@@ -0,0 +1,23 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../codeclan_student')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+class TestStudent < MiniTest::Test
+    def  test_student_name
+        student = Student.new("Craig", "E40")
+        assert_equal("Craig", student.name())
+    end
+
+    def test_student_cohort
+        student = Student.new("Zorlock", "E123")
+        assert_equal("E123", student.cohort())
+    end
+
+    def test_can_update_student_name
+        student = Student.new("Zorlock", "E123")
+        student.set_name("Farnsworth")
+        assert_equal("Farnsworth", student.name())
+    end
+
+end
